af6fc1cda27a4d6a8dfca5272405371fac7a13b2
unknown <Administrateur@ACT14.m2iformation.local>
2023-09-15 10:57:23 +0200

    config v2

--- brouillons/test1.tf
+++ brouillons/test1.tf
@@ -23,14 +23,54 @@ resource "aws_vpc" "k8s_M_vpc" {
   }
 }
 # creer les sous reseaux
-resource "aws_subnet" "subnet_K8S" {
-  count                   = 2
-  vpc_id                  = 0d04a13ed9ca964fb
-  cidr_block              = element(["10.0.1.0/24", "10.0.2.0/24"], count.index)
-  availability_zone       = element(["eu-west-3", "eu-west-3"], count.index)
-  map_public_ip_on_launch = true
+resource "aws_subnet" "Tak" {
+  vpc_id     = "vpc-0d04a13ed9ca964fb"
+  cidr_block = "10.0.1.0/24"
+
   tags = {
-    Name = "Subnet_K8s_M-${count.index}"
+    Name = "Tak-K8s"
   }
-  
+}
+ 
+resource "aws_subnet" "Mir" {
+  vpc_id     = "vpc-0d04a13ed9ca964fb"
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Mir-K8s"
+  }
+}
+
+# creation d'une internet-gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "vpc-0d04a13ed9ca964fb"
+
+  tags = {
+    Name = "K8S-int-gateway"
+  }
+}
+
+# Création de la table de routage
+resource "aws_route_table" "K8S-route_table" {
+  vpc_id = "vpc-0d04a13ed9ca964fb"
+  tags = {
+    Name = "K8S-route_table"
+  }
+}
+
+# Ajout de la route par défaut vers la gateway Internet
+resource "aws_route" "default_route" {
+  route_table_id         = "rtb-0c5443fc0902de3fe"
+  destination_cidr_block = "10.0.2.0/24"
+  gateway_id             = "igw-02b034dd6f3c58a84"
+}
+
+# Association des sous-réseaux à la table de routage
+resource "aws_route_table_association" "a" {
+  subnet_id      = "subnet-0f652f11e32f351e8"
+  route_table_id = "rtb-0c5443fc0902de3fe"
+}
+resource "aws_route_table_association" "b" {
+  subnet_id      = "subnet-07ae948a931b04d45"
+  route_table_id = "rtb-0c5443fc0902de3fe"
 }
