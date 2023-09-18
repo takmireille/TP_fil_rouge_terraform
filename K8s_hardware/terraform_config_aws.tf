5066e14fa14f6e934c0c0b4f89ba1427da19d4e5
unknown <Administrateur@ACT14.m2iformation.local>
2023-09-18 17:15:30 +0200

    definition de l'infrastructure avec terraform

--- K8s_hardware/terraform_config_aws.tf
+++ K8s_hardware/terraform_config_aws.tf
@@ -1,3 +1,4 @@
+# 
 terraform {
   required_providers {
     aws = {
@@ -6,13 +7,14 @@ terraform {
     }
   }
 }
+
 # aws credential
 provider "aws" {
   region     = "choose your region"
-  access_key = "your acces key"
-  secret_key = "your secret key"
+  access_key = "your acces_key"
+  secret_key = "your secret_key"
 }
-
+################################## network ########################################################
 # creation du vpc
 resource "aws_vpc" "k8s_M_vpc" {
   cidr_block       = "10.0.0.0/16"
@@ -68,20 +70,51 @@ resource "aws_route" "K8S-route_table" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gw.id
 }
-
+############################################################ instences ec2 (VM)################################
 # Création des clés SSH
 resource "aws_key_pair" "ssh_key" {
-  key_name   = "ssh_key"
-  public_key = file("~/.ssh/id_rsa.pub")
+  key_name   = "ssh_k8s"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCn311NHCyq7GFNrqobZ1O7XuevWQKSIgsef99zq+ZJfUvGl5akTi/HI1lyL6sNP+cQ2wU5C+pE2dfnqdNk1LTuOyQjjinISGPL94mswJ4lvVK6oeclYkmnG/mro4Yii0WuEjEWMkWlx7vA16f/o1Oxt1uOvwobxRPG71MeKJs0J1Uv/Qp/U1dnkb6ew6TiglZ8qGWAZwJpPvgjF7Y26gKKWDNqHuwuCCAYRtTa/mRo4HJQ5iw8CzyMrrZEchxZkOkj+dth2h3Su0v+fTz7JE2IFlKU4KsCPAnVgUcaL37xyaRXY99nh0wcp7zqvjHKfZu8AutnppZpi8TtzgE/vIaASs9D4bUDTmJq8aC4HGXCKSBOFtZhWHykMxaPiTprz+/cZDfGqbqQmHrB5weyauWj5t8c2mT8m7nhEM42tklqOav+zD3ouvUTnWvMrmUBLU7vGK+p1GeWILIxqwvpLef67SyUKyuzckBZYY/y7S/FBVyafhBpqybSN/UwfhX7qX8= administrateur@ACT14"
 }
-
+# Creer security group
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "Web_SG"
+  description = "Allow SSH and HTTP inbound traffic"
+  vpc_id      = aws_vpc.k8s_M_vpc.id
+  ingress {
+    description      = "Allow All"
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = [ "0.0.0.0/0" ]
+  }
+  ingress {
+    description      = "Allow All"
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = [ "0.0.0.0/0" ]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "K8S_M_SG"
+  }
+}
 
 # Création de l'instance master
 resource "aws_instance" "master_instance" {
   ami           = "ami-05b5a865c3579bbc4" 
   instance_type = "t2.micro" 
+  security_groups = [ aws_security_group.allow_ssh_http.id]
   subnet_id     = aws_subnet.Mir.id
-  key_name      ="ssh_key"
+  key_name      = aws_key_pair.ssh_key.key_name
+  associate_public_ip_address = true
   tags = {
     Name = "Master-k8s-Instance"
   }
@@ -89,10 +122,12 @@ resource "aws_instance" "master_instance" {
 
 # Création de l'instance worker
 resource "aws_instance" "worker_instance" {
-  ami           = "ami-05b5a865c3579bbc4" 
+  ami           = "ami-05b5a865c3579bbc4"
   instance_type = "t2.micro" 
+  security_groups = [ aws_security_group.allow_ssh_http.id ]
   subnet_id     = aws_subnet.Mir.id
-  key_name      = "ssh_key"
+  key_name      = aws_key_pair.ssh_key.key_name
+  associate_public_ip_address = true
   tags = {
      Name = "Worker-k8-Instance"
   } 
